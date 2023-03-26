09564913f2b00eb02efea1efc2ba89416370073b
tts2k <tranthaison2000@gmail.com>
2023-03-26 22:06:57 +0700

    nvim: show macro status on statusline

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,8 +1,18 @@
 local M = {
   'nvim-lualine/lualine.nvim',
-  config = true,
+  opts = {
+    sections = {
+      lualine_x = {
+        {
+          require("noice").api.statusline.mode.get,
+          cond = require("noice").api.statusline.mode.has,
+        }
+      }
+    }
+  },
   dependencies = {
-    'nvim-tree/nvim-web-devicons'
+    'nvim-tree/nvim-web-devicons',
+    'folke/noice.nvim',
   }
 }
 return M
